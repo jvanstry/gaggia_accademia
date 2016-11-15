ebdd910e9e8359e38b1b313bf6ffdc7544c9eb31
Jerry VanStrydonck <jvanstry@gmail.com>
2016-11-15 18:24:11 -0500

    updated package.json and added nodemon and corresponding gulptask for live reload

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,49 @@
 var gulp = require('gulp');
+var jasmineBrowser = require('gulp-jasmine-browser');
 var less = require('gulp-less');
-var path = require('path');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
-var jasmineBrowser = require('gulp-jasmine-browser');
+var nodemon = require('gulp-nodemon');
+var path = require('path');
+
+gulp.task('default', ['less', 'browser-sync'], function () {
+    gulp.watch("public/styles/less/*.less", ['less', reload]);
+    gulp.watch(["public/scripts/*.js", "views/**/*.ejs"], reload);
+});
 
 gulp.task('less', function() {
-  return gulp.src('./app/styles/less/*.less')
+  return gulp.src('public/styles/less/*.less')
     .pipe(less({
       paths: [ path.join(__dirname, 'less', 'includes') ]
     }))
-    .pipe(gulp.dest('./app/styles/css'));
+    .pipe(gulp.dest('public/styles/css'));
 });
 
-gulp.task('serve', ['less'], function() {
-  browserSync({
-    server: {
-      baseDir: 'app'
-    }
+gulp.task('browser-sync', ['nodemon'], function() {
+  browserSync.init(null, {
+    proxy: "http://localhost:8080",
+        files: ["public/**/*.*"],
+        browser: "google chrome",
+        port: 7000,
   });
-
-  gulp.watch(['*.html', 'styles/**/*.css', 'styles/**/*.less', 'scripts/**/*.js'], {cwd: 'app'}, reload);
 });
- 
+
+gulp.task('nodemon', function (cb) {
+  
+  var started = false;
+  
+  return nodemon({
+    script: 'app.js'
+  }).on('start', function () {
+    if (!started) {
+      cb();
+      started = true; 
+    } 
+  });
+});
 
 gulp.task('test', function() {
-  return gulp.src(['app/**/*.js', 'spec/specHelper.js', 'spec/**/*_spec.js'])
+  return gulp.src(['public/scripts/*.js', 'spec/specHelper.js', 'spec/**/*_spec.js'])
     .pipe(jasmineBrowser.specRunner({console: true}))
     .pipe(jasmineBrowser.headless());
 });

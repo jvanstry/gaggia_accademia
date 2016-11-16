203c531902997bf63ffd2dd0547037c7bacaacb9
Jerry VanStrydonck <jvanstry@gmail.com>
2016-11-16 04:59:14 -0500

    a lot of work done

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
 var applicationRoot = __dirname;
-var pkg = require('./package.json');
-var config = require('./config.json');
 var path = require('path');
 var fs = require('fs');
-var express = require('express');
 
+var productData = JSON.parse(fs.readFileSync('data.json', 'utf8'));
+
+var express = require('express');
 var app = express();
-var port = process.env.port || 8080; 
+var port = process.env.port || 8080;
 
 app.set('view engine', 'ejs');
 app.use(express.static(applicationRoot + '/public'));
 
 app.get('/', function(req, res) {
-    res.render('pages/index', {
-      title: 'JVS - Gaggia Accademia Exercise'
-    });
+  var abbrevDesc = productData.overview["Product Description"].substr(0, 410) + "...";
+
+  res.render('pages/index', {
+    title: 'JVS - Gaggia Accademia Exercise',
+    productData: productData,
+    abbreviatedDescription: abbrevDesc
+  });
 });
 
 app.listen(port);
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,1 +1,37 @@
-var hello = "jerry";
+$(document).ready(function(){
+  console.log('hi')
+
+  $('.dead-link').click(function(e){
+    e.preventDefault();
+  });
+
+  $('.jumpto').click(function(e){
+    e.preventDefault();
+    var targetElement;
+
+    switch(e.currentTarget.classList[0]){
+      case 'jumpto-reviews':
+        targetElement = '#reviews'
+        break;
+      case 'jumpto-desc':
+        targetElement = '#description'
+        break;
+      case 'jumpto-specs':
+        targetElement = '#specifications'
+        break;
+      case 'jumpto-related':
+        targetElement = '#related-products';
+        break;
+      case 'jumpto-videos':
+        targetElement = '#videos'
+        break
+      default:
+        targetElement = '#product'
+        break;
+    }
+
+    $('html, body').animate({
+        scrollTop: $(targetElement).offset().top
+    }, 1000);
+  });
+})
